d6d30eeb5590a28581de7d3312b6db130f350fd8
issgg <israelga2000@gmail.com>
2025-05-13 01:06:12 -0700

    este es otro cambio

--- archivo.cpp
+++ archivo.cpp
@@ -2,5 +2,9 @@
 int main(){
 
     printf("gael te la quiero chupar hasta que te salga en polvo y hacerme un chocolate:333333333333");
+    
+
+
+    printf("Este es otro cambio xd");
     return 1;
 }
